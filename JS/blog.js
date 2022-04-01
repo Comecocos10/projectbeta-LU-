35ec838140da6efd33687cc47da4555c71530595
Comecocos10 <ahp93524@gmail.com>
2022-04-01 18:58:29 +0200

    Blog + Retoques

--- JS/blog.js
+++ JS/blog.js
@@ -1,8 +1,34 @@
 function showElementBlog() {
-    const check = document.querySelector('.desplegable');
+    const check = document.querySelector('.checkbox-desplegable');
     if (check.checked === true) {
         document.querySelector('.cuadro-desplegable-detras').style.display = 'block';
     } else {
         document.querySelector('.cuadro-desplegable-detras').style.display = 'none';
     }
 }
+
+function showElementBoton(){
+
+    function showElementDown() {
+        const check = document.querySelector('.checkbox-desplegable');
+        if (check.checked === true) {
+            document.querySelector('.down').style.display = 'none';
+        } else {
+            document.querySelector('.down').style.display = 'block';
+        }
+    }
+
+    function showElementUp() {
+        const check = document.querySelector('.checkbox-desplegable');
+        if (check.checked === true) {
+            document.querySelector('.up').style.display = 'block';
+        } else {
+            document.querySelector('.up').style.display = 'none';
+        }
+    }
+
+}
+
+
+
+
