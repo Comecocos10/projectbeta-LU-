1ae55b85ea50bf949aa90e925e32a35916c644d6
Comecocos10 <ahp93524@gmail.com>
2022-04-02 15:54:00 +0200

    blog

--- JS/blog.js
+++ JS/blog.js
@@ -2,14 +2,14 @@ function showElementBlog() {
     const check = document.querySelector('.checkbox-desplegable');
     if (check.checked === true) {
         document.querySelector('.cuadro-desplegable-detras').style.display = 'block';
+        document.querySelector('.fondo-desplegable').style.display = 'block';
     } else {
         document.querySelector('.cuadro-desplegable-detras').style.display = 'none';
+        document.querySelector('.fondo-desplegable').style.display = 'none';
     }
 }
 
 function showElementBoton(){
-
-
         const check = document.querySelector('.checkbox-desplegable');
         if (check.checked === true) {
             document.querySelector('.icon-blog').classList.add('up');
